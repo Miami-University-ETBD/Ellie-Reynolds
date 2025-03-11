a844cc205e451829bfdbe719ab28b6772dce2d46
ellie-reynolds <reynoleg@miamioh.edu>
2025-03-11 16:39:25 -0400

    fixed comments

---
+++ script.js
@@ -0,0 +1,13 @@
+    // JavaScript Interaction 1: Button click to show an alert
+    function showMessage() {
+        alert("Thank you for visiting Ellie Reynolds Photography!");
+    }
+
+    // JavaScript Interaction 2: Change text on mouseover
+    function changeText(element) {
+        element.innerHTML = "Let's Connect!";
+    }
+
+    function resetText(element) {
+        element.innerHTML = "Contact Me!";
+    }
